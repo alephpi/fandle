a22c9e50f199fe16d7853af6803088388cf972f1
alephpi <maosicheng98@gmail.com>
2023-06-08 22:11:14 +0200

    feat: freeze entire answer sheet

--- src/answers/index.ts
+++ src/answers/index.ts
@@ -1,13 +1,23 @@
+import seedrandom from 'seedrandom'
 import type { DictType } from '../logic'
 import { getHint } from '../logic'
 import { answers as answersMandarin } from './list_mandarin'
 import { answers as answersCantonese } from './list_cantonese'
 export function getAnswerOfDay(day: number, mode: DictType) {
   let [word, hint] = ['', '']
-  if (mode === 'cantonese')
-    [word = '', hint = ''] = answersCantonese[day] || []
-  else
-    [word = '', hint = ''] = answersMandarin[day] || []
+  const seed = seedrandom(`day-${day}`)()
+  if (mode === 'cantonese') {
+    if (day > answersCantonese.length)
+      [word = '', hint = ''] = answersCantonese[Math.floor(seed * answersCantonese.length)] || []
+    else
+      [word = '', hint = ''] = answersCantonese[day] || []
+  }
+  else {
+    if (day > answersMandarin.length)
+      [word = '', hint = ''] = answersMandarin[Math.floor(seed * answersMandarin.length)] || []
+    else
+      [word = '', hint = ''] = answersMandarin[day] || []
+  }
   return {
     word,
     hint: hint || getHint(word),
--- src/answers/list_cantonese.ts
+++ src/answers/list_cantonese.ts
@@ -40,158 +40,8 @@
  *
  *
  */
-
-import { seedShuffle } from './utils'
-
-function prepare<T>(len: number, arr: T[]) {
-  if (import.meta.hot && arr.length !== len)
-    throw new Error(`expect array length ${arr.length} to be ${len}`)
-  return arr
-}
-
-export const _PRE = prepare(1, [[]])
-
-export const _2022_SEP = prepare(30, seedShuffle([
-  ['naan4', ''],
-  ['kyun4', ''],
-  ['ding2', ''],
-  ['ceon1', ''],
-  ['daai2', ''],
-  ['zok6', ''],
-  ['hip3', ''],
-  ['zuk6', ''],
-  ['long5', ''],
-  ['gwai3', ''],
-  ['jau3', ''],
-  ['coeng4', ''],
-  ['wu1', ''],
-  ['giu2', ''],
-  ['giu1', ''],
-  ['fei1', ''],
-  ['fu4', ''],
-  ['seoi2', ''],
-  ['zim6', ''],
-  ['ngaan6', ''],
-  ['lau4', ''],
-  ['fui3', ''],
-  ['gou3', ''],
-  ['zeon3', ''],
-  ['coi4', ''],
-  ['gwo1', ''],
-  ['doi6', ''],
-  ['caap3', ''],
-  ['sin3', ''],
-  ['tim1', ''],
-], '2022-09'))
-
-export const _2022_OCT = prepare(31, seedShuffle([
-  ['naan4', ''],
-  ['kyun4', ''],
-  ['ding2', ''],
-  ['ceon1', ''],
-  ['daai2', ''],
-  ['zok6', ''],
-  ['hip3', ''],
-  ['zuk6', ''],
-  ['long5', ''],
-  ['gwai3', ''],
-  ['jau3', ''],
-  ['coeng4', ''],
-  ['wu1', ''],
-  ['giu2', ''],
-  ['giu1', ''],
-  ['fei1', ''],
-  ['fu4', ''],
-  ['seoi2', ''],
-  ['zim6', ''],
-  ['ngaan6', ''],
-  ['lau4', ''],
-  ['fui3', ''],
-  ['gou3', ''],
-  ['zeon3', ''],
-  ['coi4', ''],
-  ['gwo1', ''],
-  ['doi6', ''],
-  ['caap3', ''],
-  ['sin3', ''],
-  ['tim1', ''],
-  ['koeng4', ''],
-], '2022-10'))
-
-export const _2022_NOV = prepare(30, seedShuffle([
-  ['zau1', ''],
-  ['huk6', ''],
-  ['gwaan1', ''],
-  ['kêu5', ''],
-  ['hou2', ''],
-  ['sau2', ''],
-  ['zing6', ''],
-  ['jim6', ''],
-  ['zêu3', ''],
-  ['saai2', ''],
-  ['hon6', ''],
-  ['pou2', ''],
-  ['tiu1', ''],
-  ['mou4', ''],
-  ['cam4', ''],
-  ['gaai1', ''],
-  ['bin6', ''],
-  ['joek3', ''],
-  ['baan1', ''],
-  ['lin4', ''],
-  ['coi3', ''],
-  ['tai4', ''],
-  ['mo1', ''],
-  ['gaam2', ''],
-  ['wut6', ''],
-  ['zing1', ''],
-  ['dyun2', ''],
-  ['leon6', ''],
-  ['loi4', ''],
-  ['hok6', ''],
-], '2022-11'))
-
-export const _2022_DEC = prepare(31, seedShuffle([
-  ['cyu5', ''],
-  ['waa6', ''],
-  ['soek3', ''],
-  ['hon3', ''],
-  ['jyu1', ''],
-  ['jung6', ''],
-  ['??', ''],
-  ['ji5', ''],
-  ['ning4', ''],
-  ['oi3', ''],
-  ['gaau2', ''],
-  ['jing1', ''],
-  ['mai6', ''],
-  ['sap6', ''],
-  ['caak3', ''],
-  ['jing3', ''],
-  ['fong4', ''],
-  ['ngai4', ''],
-  ['sung3', ''],
-  ['si2', ''],
-  ['sin1', ''],
-  ['gaai3', ''],
-  ['zyu3', ''],
-  ['hyun2', ''],
-  ['gaan1', ''],
-  ['gan2', ''],
-  ['kiu2', ''],
-  ['zang1', ''],
-  ['bat6', ''],
-  ['si5', ''],
-  ['sap1', ''],
-], '2022-12'))
-
-export const answers: string[][] = [
-  ..._PRE,
-  ..._2022_SEP,
-  ..._2022_OCT,
-  ..._2022_NOV,
-  ..._2022_DEC,
-]
+import { answer_list_cantonese } from './answer_list_raw_cantonese'
+export const answers = answer_list_cantonese
 
 // duplicated check
 if (import.meta.hot) {
@@ -203,10 +53,10 @@ if (import.meta.hot) {
       return
     if (!map.has(a[0]))
       map.set(a[0], i)
-    else
-      // throw new Error(`Word ${a[0]} is duplicated at ${map.get(a[0])}`)
-      if (a[1] && !a[0].includes(a[1]))
-        throw new Error(`Hint ${a[1]} is not included in ${a[0]}`)
+    // else
+    // throw new Error(`Word ${a[0]} is duplicated at ${map.get(a[0])}`)
+    if (a[1] && !a[0].includes(a[1]))
+      throw new Error(`Hint ${a[1]} is not included in ${a[0]}`)
     if (!checkValidPinyin(a[0], 'cantonese'))
       throw new Error(`${a[0]} is not a valid pinyin`)
   })
